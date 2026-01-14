c170485fb5d6908f18032c991fe5962e8025a987
jyncodes <pjanereyes14@gmail.com>
2026-01-14 15:07:43 +0800

    css blocking the button

--- joven/src/pages/user-page/ARPage.jsx
+++ joven/src/pages/user-page/ARPage.jsx
@@ -25,12 +25,10 @@ const ARPage = () => {
   /* 🔒 Disable scroll & gestures */
   useEffect(() => {
     document.body.style.overflow = "hidden";
-    document.body.style.touchAction = "none";
     document.documentElement.style.overflow = "hidden";
 
     return () => {
       document.body.style.overflow = "";
-      document.body.style.touchAction = "";
       document.documentElement.style.overflow = "";
     };
   }, []);
